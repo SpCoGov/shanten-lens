31938422016a4e06bb488f741f4046139dcafc36
SpCoGov <haimiancouna@gmail.com>
2026-03-31 22:19:20 +0800

    complete missing translation

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {invoke} from "@tauri-apps/api/core";
-import {createRoot} from "react-dom/client";
 import App from "./App";
 import { ensureI18nReady } from "./lib/i18n";
 
 invoke("frontend_ready").catch(() => {
 });
-createRoot(document.getElementById("root")!).render(<App/>);
-
 ensureI18nReady().then(() => {
     ReactDOM.createRoot(document.getElementById("root")!).render(
         <React.StrictMode>
